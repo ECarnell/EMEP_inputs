7398cbbf1a4539b44ae92d73e2e39e529f6e1885
Tomlinson <samtom@ceh.ac.uk>
2020-07-03 15:01:16 +0100

    tweak

--- EMEP_UK_inputs.R
+++ EMEP_UK_inputs.R
@@ -1,9 +1,7 @@
 setwd("//nercbuctdb.ad.nerc.ac.uk/projects1/NEC03642_Mapping_Ag_Emissions_AC0112/NAEI_data_and_SNAPS")
 
-
-
 source("N:/dump/EMEP_inputs/workspace.R")
-source("N:/dump/EMEP_inputs/emissions_functions.R")
+source("C:/FastProcessingSam/Git_repos/EMEP_inputs/emissions_functions.R")
 
 ###########################################################
 ####                                                   ####
@@ -16,8 +14,8 @@ source("N:/dump/EMEP_inputs/emissions_functions.R")
 ###########################################################
 
 ### CHOOSE WHICH YEARS AND WHICH POLLUTANTS/GHGS TO PUT IN THE NETCDF ###
-years <- 2016:2017
-pollutants <- c("nox","sox","","","","")
+years <- 2010
+pollutants <- c("nox")
 mapping.yr <- 2017 # i.e. what year is the NAEI spatial distribution for the data
 region <- "ukeire" # 'uk' = UK only, 'eire' = Eire only, 'ukeire' = UK and Eire combined
 class <- "GNFR" # Sector classification system: 'SNAP' or 'GNFR'
@@ -30,12 +28,12 @@ yr.poll.subset <- check.netcdf.status(years = years, pollutants = pollutants, ma
 # 2. For the remaining years & pollutants, take the regional emissions in Lat Long and;
        #   i) convert point emissions (.csv) into a raster
        #  ii) combine the points with the diffuse (.tif) data as required for the model 
+       # iii) For SNAP 1: ONLY point sources go into A_PublicPower as this is treated with 200m injection. All other into B_Industry.
 
-pt.diff.data <- combine.all.region.data(yr.poll.subset = yr.poll.subset, mapping.yr = mapping.yr, class = class, region = region) # c. 2.5 mins per year & pollutant
+combined.data <- combine.all.region.data(yr.poll.subset = yr.poll.subset, mapping.yr = mapping.yr, class = class, region = region) # c. 2.5 mins per year & pollutant
 
 # 3. Using the newly classified GNFR data, place into a netcdf (either existing or new)
-input.to.netcdf(pt.diff.data = pt.diff.data, years = unique(yr.poll.subset[,years]), pollutants = unique(yr.poll.subset[,pollutants]), yr.poll.subset = yr.poll.subset, mapping.yr = mapping.yr, class = class, region = region)
+input.to.netcdf(combined.data = combined.data, years = unique(yr.poll.subset[,years]), pollutants = unique(yr.poll.subset[,pollutants]), yr.poll.subset = yr.poll.subset, mapping.yr = mapping.yr, class = class, region = region)
 
 
 ########################################
-
